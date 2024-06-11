7fd18b368fdf7d20ddf90c17fc5096f8eda9ef09
fazan <fasafazan@gmail.com>
2024-06-11 10:51:23 +0530

    Encrypt and Decrypt Passwords

---
+++ PasswordEncryptor/PasswordManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordEncryptor
+{
+    public class PasswordManager
+    {
+   
+        
+
+        public static string Encrypt(string plaintext, int shift)
+        {
+            string encryptedText = "";
+            foreach (char c in plaintext)
+            {
+                if (char.IsLetter(c))
+                {
+                    char offset = char.IsUpper(c) ? 'A' : 'a';
+                    encryptedText += (char)(((c + shift - offset) % 26) + offset);
+                }
+                else
+                {
+                    encryptedText += c; // Non-letter characters are unchanged
+                }
+            }
+            return encryptedText;
+        }
+        public static string Decrypt(string ciphertext, int shift)
+        {
+            return Encrypt(ciphertext, 26 - shift); // Decryption is just encryption with 26 - shift
+        }
+
+
+
+    }
+}
